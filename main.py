da08c625140c3d7b7e2eeb3cf875bfee8e094f2b
sauravtom <stomatrix@gmail.com>
2015-03-22 04:58:07 +0530

    code cleanup

--- main.py
+++ main.py
@@ -15,68 +15,14 @@ def get_dict(**kwargs):
         d[k] = v
     return d
 
-def scrape_service():
-    with open("data/service.json",'r') as f:
-        data = f.read()
-        data = json.loads(data)
-
-    arr = []
-    for i in data["data"]:
-        lat_lang=geocode_(i[0]+" "+i[1])
-        if not lat_lang:
-            continue
-        dict = get_dict(name=i[0],coords=lat_lang,meta="Phone: %s , Contact: %s , Mail: %s"%(i[2],i[8],i[4]),rating="")
-        arr.append(dict)
-        print dict
-
-    with open("data/service__.json",'w+') as f:
-        f.write(json.dumps(arr))
-    
-    return ""
-
-def scrape_hotels():
-    with open("data/hotels.json",'r') as f:
-        data = f.read()
-        data = json.loads(data)
-
-    arr = []
-    for i in data["data"]:
-        lat_lang=geocode_(i[0]+" "+i[1])
-        if not lat_lang:
-            continue
-        dict = get_dict(name=i[0],coords=lat_lang,meta="Phone: %s ,Mail: %s , Type: %s, Rooms: %s"%(i[3],i[5],i[7],i[8]),rating="")
-        arr.append(dict)
-        print dict
-
-    with open("data/hotels__.json",'w+') as f:
-        f.write(json.dumps(arr))
-    
-    return ""
-
-def geocode_(address):
-    geocoding_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=AIzaSyDmUUwc54w3uoNlGXenMR5oNzm3qTRVbtY"%address
-    r = requests.get(geocoding_url).json()
-    #coords = r["results"]["geometry"]["location"]
-    try:
-        coords = r["results"][0]["geometry"]["location"]
-    except IndexError:
-        coords=""
-    return coords
 
 @app.route('/', methods=['GET'])
 def home():
     map_long = request.args.get('long')
     map_lat = request.args.get('lat')
     map_type = request.args.get('type')
-
-    with open("data/hotels__.json",'r') as f:
-        data = json.loads(f.read())
-
-    for i in data:
-        print i
-        i['dist'] = math.hypot(float(map_lat) - float(i['coords']['lat']), float(map_long) - float(i['coords']['lng']))
-
-    hotels = sorted(data, key=lambda k: k['dist']) 
+    radius = request.args.get('radius') or 2000
+    map_type.replace(",","|")
 
     with open("data/service__.json",'r') as f:
         data = json.loads(f.read())
@@ -87,8 +33,8 @@ def home():
 
     services = sorted(data, key=lambda k: k['dist']) 
     
-    url = "https://maps.googleapis.com/maps/api/place/search/json?types=%s&location=%s,%s&radius=5000&key=AIzaSyDLKMcL0I-b3X_jOiCtdjEI2hDkT5B6J8g"%(map_type,map_lat,map_long)
-    #return url
+    url = "https://maps.googleapis.com/maps/api/place/search/json?types=%s&location=%s,%s&radius=%s&key=AIzaSyDLKMcL0I-b3X_jOiCtdjEI2hDkT5B6J8g"%(map_type,map_lat,map_long,radius)
+    
     r = requests.get(url).json()['results']
     arr=[]
     for i in r:
@@ -100,32 +46,37 @@ def home():
                 coords=i['geometry']['location'],meta="")
         arr.append(dict)
 
+    if 'hotels' not in map_type:
+        return jsonify(data = arr,services=services)
+
+    with open("data/hotels__.json",'r') as f:
+        data = json.loads(f.read())
+
+    for i in data:
+        print i
+        i['dist'] = math.hypot(float(map_lat) - float(i['coords']['lat']), float(map_long) - float(i['coords']['lng']))
+
+    hotels = sorted(data, key=lambda k: k['dist'])
+
     return jsonify(data = arr,hotels=hotels,services=services)
+        
+
+@app.route('/heat',methods=['GET'])
+def heat():
+    address = request.args.get('address')
+    return json.dumps(geocode_(address))
+
+@app.route('/adler',methods=['GET'])
+def question():
+    q = request.args.get('q')
+    return jsonify(response="Hello World")
 
 @app.route('/geocode',methods=['GET'])
 def geocode():
     address = request.args.get('address')
     return json.dumps(geocode_(address))
 
-@app.route('/adler',methods=['GET'])
-def question():
-    q= request.args.get('q')
-    return jsonify(response="Hello World")
     
-
-def wattson():
-    qt = 'How deep is grand canyon ?'
-    url ="https://gateway.watsonplatform.net/question-and-answer-beta/api/v1/question/travel"
-    r = requests.post(url,
-        data={'question':{'questionText':qt,'evidenceRequest':{'items':1}}},
-        headers={'Accept':'application/json','Content-Type':'text/json','X-SyncTimeout':'30'},
-        auth={'fe4fa332-50cb-4c91-9ce5-84122b753824','swKJrtrfi8fS'}
-        )
-    print r
-    try:
-        print r.text
-    except:
-        pass
 
 
 if __name__ == '__main__':
---
+++ scraper.py
@@ -0,0 +1,118 @@
+
+from bs4 import BeautifulSoup
+import requests
+import json
+
+def scrape_service():
+    with open("data/service.json",'r') as f:
+        data = f.read()
+        data = json.loads(data)
+
+    arr = []
+    for i in data["data"]:
+        lat_lang=geocode_(i[0]+" "+i[1])
+        if not lat_lang:
+            continue
+        dict = get_dict(name=i[0],coords=lat_lang,meta="Phone: %s , Contact: %s , Mail: %s"%(i[2],i[8],i[4]),rating="")
+        arr.append(dict)
+        print dict
+
+    with open("data/service__.json",'w+') as f:
+        f.write(json.dumps(arr))
+    
+    return ""
+
+def scrape_hotels():
+    with open("data/hotels.json",'r') as f:
+        data = f.read()
+        data = json.loads(data)
+
+    arr = []
+    for i in data["data"]:
+        lat_lang=geocode_(i[0]+" "+i[1])
+        if not lat_lang:
+            continue
+        dict = get_dict(name=i[0],coords=lat_lang,meta="Phone: %s ,Mail: %s , Type: %s, Rooms: %s"%(i[3],i[5],i[7],i[8]),rating="")
+        arr.append(dict)
+        print dict
+
+    with open("data/hotels__.json",'w+') as f:
+        f.write(json.dumps(arr))
+    
+    return ""
+
+def geocode_(address):
+    geocoding_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=AIzaSyDPORyFiuMXkIgO0hniehw-XCZf5Qp9XSQ"%address
+    r = requests.get(geocoding_url).json()
+    #coords = r["results"]["geometry"]["location"]
+    try:
+        coords = r["results"][0]["geometry"]["location"]
+    except IndexError:
+        coords=""
+
+
+    return coords
+
+def get_dict(**kwargs):
+    d= {}
+    for k,v in kwargs.iteritems():
+        d[k] = v
+    return d
+
+def scrape_census():
+	url ='http://www.census2011.co.in/district.php'
+	soup = BeautifulSoup( requests.get(url).text )
+	main_arr=[]
+	for i in soup.select(".table .row"):
+		for j in i.select(".cell")[1:]:
+			try:
+				text = i.get_text()
+				a=[i for i in text.split("\n") if i][1:]
+				main_arr.append(a)
+			except AttributeError:
+				pass
+
+	arr = []
+	for i in main_arr:
+		coords= geocode_(i[0]+" "+i[1])
+		#coords=""
+		d = get_dict(coords=coords,district=i[0],state=i[1],population=i[2],growth=i[3],sex_ratio=i[4],literacy=i[5])
+		arr.append(d)
+		#print d
+
+	print json.dumps(arr)
+	with open('data/heat1.json','w+') as f:
+	    f.write(json.dumps(arr))
+
+def adler():
+    qt = 'How deep is grand canyon ?'
+    url ="https://gateway.watsonplatform.net/question-and-answer-beta/api/v1/question/travel"
+    r = requests.post(url,
+        data=json.dumps({'question':{'questionText':qt,'evidenceRequest':{'items':1}}}),
+        headers={'Accept':'application/json','Content-Type':'text/json','X-SyncTimeout':'30'},
+        auth=('fe4fa332-50cb-4c91-9ce5-84122b753824','swKJrtrfi8fS')
+        )
+    try:
+        print r.text
+    except:
+        pass
+
+def foo():
+	with open("data/heat1.json") as f:
+		data = json.loads(f.read())
+	max = 0
+	for i in data:
+		p = int(i['population'].replace(',',""))
+		if p>max:
+			max = p
+	for i in data:
+		i['percent'] = int(i['population'].replace(",",""))/float(max)
+		print i,i['percent'],max
+
+	print data
+
+
+if __name__ == '__main__':
+	#adler()
+	foo()
+	#scrape_census()
