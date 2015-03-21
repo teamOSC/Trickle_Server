08edf28c83dbe3d80f90e464cb6e55545b2fa681
sauravtom <stomatrix@gmail.com>
2015-03-22 02:25:39 +0530

    added hotels and services in the API

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import json
 import requests
 from flask import jsonify
+import math
 
 app = flask.Flask(__name__)
 
@@ -14,27 +15,46 @@ def get_dict(**kwargs):
         d[k] = v
     return d
 
-def scrape():
+def scrape_service():
+    with open("data/service.json",'r') as f:
+        data = f.read()
+        data = json.loads(data)
+
+    arr = []
+    for i in data["data"]:
+        lat_lang=geocode_(i[0]+" "+i[1])
+        if not lat_lang:
+            continue
+        dict = get_dict(name=i[0],coords=lat_lang,meta="Phone: %s , Contact: %s , Mail: %s"%(i[2],i[8],i[4]),rating="")
+        arr.append(dict)
+        print dict
+
+    with open("data/service__.json",'w+') as f:
+        f.write(json.dumps(arr))
+    
+    return ""
+
+def scrape_hotels():
     with open("data/hotels.json",'r') as f:
         data = f.read()
         data = json.loads(data)
 
     arr = []
     for i in data["data"]:
-        i.append(geocode_(i[0]+" "+i[1]))
-        dict = get_dict(name=i[1],address=i[2],state=i[3],phone_1=i[4],phone_2=i[5],\
-        mail_1=i[6],mail_2=i[7],type=i[8],rooms=i[9])
+        lat_lang=geocode_(i[0]+" "+i[1])
+        if not lat_lang:
+            continue
+        dict = get_dict(name=i[0],coords=lat_lang,meta="Phone: %s ,Mail: %s , Type: %s, Rooms: %s"%(i[3],i[5],i[7],i[8]),rating="")
         arr.append(dict)
         print dict
 
-    with open("data/hotels_.json",'w+') as f:
+    with open("data/hotels__.json",'w+') as f:
         f.write(json.dumps(arr))
     
     return ""
 
-
 def geocode_(address):
-    geocoding_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=AIzaSyAnFWncNFX_cgSOqUUR7u5udupNPrkQ0Ro"%address
+    geocoding_url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=AIzaSyDmUUwc54w3uoNlGXenMR5oNzm3qTRVbtY"%address
     r = requests.get(geocoding_url).json()
     #coords = r["results"]["geometry"]["location"]
     try:
@@ -48,17 +68,30 @@ def home():
     map_long = request.args.get('long')
     map_lat = request.args.get('lat')
     map_type = request.args.get('type')
-    with open("data/hotels_.json",'r') as f:
-        data = f.read()
 
-    url = "https://maps.googleapis.com/maps/api/place/search/json?types=%s\
-&location=%s,%s&radius=5000&key=\
-AIzaSyDLKMcL0I-b3X_jOiCtdjEI2hDkT5B6J8g"%(map_type,map_lat,map_long)
+    with open("data/hotels__.json",'r') as f:
+        data = json.loads(f.read())
+
+    for i in data:
+        print i
+        i['dist'] = math.hypot(float(map_lat) - float(i['coords']['lat']), float(map_long) - float(i['coords']['lng']))
+
+    hotels = sorted(data, key=lambda k: k['dist']) 
+
+    with open("data/service__.json",'r') as f:
+        data = json.loads(f.read())
+
+    for i in data:
+        print i
+        i['dist'] = math.hypot(float(map_lat) - float(i['coords']['lat']), float(map_long) - float(i['coords']['lng']))
+
+    services = sorted(data, key=lambda k: k['dist']) 
+    
+    url = "https://maps.googleapis.com/maps/api/place/search/json?types=%s&location=%s,%s&radius=5000&key=AIzaSyDLKMcL0I-b3X_jOiCtdjEI2hDkT5B6J8g"%(map_type,map_lat,map_long)
     #return url
     r = requests.get(url).json()['results']
     arr=[]
     for i in r:
-        print i
         try:
             dict = get_dict(rating=i['rating'],name=i['name'],
                 coords=i['geometry']['location'],meta="")
@@ -67,8 +100,7 @@ AIzaSyDLKMcL0I-b3X_jOiCtdjEI2hDkT5B6J8g"%(map_type,map_lat,map_long)
                 coords=i['geometry']['location'],meta="")
         arr.append(dict)
 
-    return jsonify(data = arr)
-    return json.dumps(arr)
+    return jsonify(data = arr,hotels=hotels,services=services)
 
 @app.route('/geocode',methods=['GET'])
 def geocode():
@@ -86,9 +118,10 @@ def question():
         auth={'fe4fa332-50cb-4c91-9ce5-84122b753824','swKJrtrfi8fS'},
         )
     print r.text
-    return ""
+    return jsonify(response="Hello World")
+    
 
-def test2():
+def wattson():
     qt = 'How deep is grand canyon ?'
     url ="https://gateway.watsonplatform.net/question-and-answer-beta/api/v1/question/travel"
     r = requests.post(url,
@@ -102,24 +135,8 @@ def test2():
     except:
         pass
 
-def test():
-    qt = 'How deep is grand canyon ?'
-    url ="https://gateway.watsonplatform.net/question-and-answer-beta/api/v1/ping"
-    r = requests.get(url,auth=('278dc7a7-4997-4105-91b8-d190d0d81b71','Wgt3j8IMcOl0'))
-    print r.text
-    return r.text
-    r = requests.post(url,
-        data={'question':{'questionText':qt,'evidenceRequest':{'items':1}}},
-        headers={'Accept':'application/json','Content-Type':'text/json','X-SyncTimeout':'30'},
-        auth=('278dc7a7-4997-4105-91b8-d190d0d81b71','Wgt3j8IMcOl0')
-        )
-    print r
-    try:
-        print r.text
-    except:
-        pass
 
 if __name__ == '__main__':
-    #test2()
+    #scrape_service()
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
