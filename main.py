6fcda4e0cb5ad26ada773fee06e566d51ccd1898
sauravtom <stomatrix@gmail.com>
2015-03-22 00:14:50 +0530

    added api

--- main.py
+++ main.py
@@ -46,17 +46,23 @@ def geocode_(address):
 def home():
     map_long = request.args.get('long')
     map_lat = request.args.get('lat')
-    with open("data/hotels.json",'r') as f:
+    map_type = request.args.get('type')
+    with open("data/hotels_.json",'r') as f:
         data = f.read()
 
-    return json.dumps(data)
+    url = "https://maps.googleapis.com/maps/api/place/search/json?types=%s\
+&location=%s,%s&radius=5000&key=\
+AIzaSyDLKMcL0I-b3X_jOiCtdjEI2hDkT5B6J8g"%(map_type,map_lat,map_long)
+    #return url
+    r = requests.get(url).json()['results']
+    return json.dumps(r)
 
 @app.route('/geocode',methods=['GET'])
 def geocode():
     address = request.args.get('address')
     return json.dumps(geocode_(address))
 
-@app.route('/qa',methods=['GET'])
+@app.route('/assistant',methods=['GET'])
 def question():
     qt = 'How far is kashmir from Delhi ?'
     headers={}
@@ -69,14 +75,13 @@ def question():
     print r.text
     return ""
 
-def test():
+def test2():
     qt = 'How deep is grand canyon ?'
-    headers={}
     url ="https://gateway.watsonplatform.net/question-and-answer-beta/api/v1/question/travel"
     r = requests.post(url,
         data={'question':{'questionText':qt,'evidenceRequest':{'items':1}}},
         headers={'Accept':'application/json','Content-Type':'text/json','X-SyncTimeout':'30'},
-        auth=('fe4fa332-50cb-4c91-9ce5-84122b753824','swKJrtrfi8fS')
+        auth={'fe4fa332-50cb-4c91-9ce5-84122b753824','swKJrtrfi8fS'}
         )
     print r
     try:
@@ -84,8 +89,24 @@ def test():
     except:
         pass
 
+def test():
+    qt = 'How deep is grand canyon ?'
+    url ="https://gateway.watsonplatform.net/question-and-answer-beta/api/v1/ping"
+    r = requests.get(url,auth=('278dc7a7-4997-4105-91b8-d190d0d81b71','Wgt3j8IMcOl0'))
+    print r.text
+    return r.text
+    r = requests.post(url,
+        data={'question':{'questionText':qt,'evidenceRequest':{'items':1}}},
+        headers={'Accept':'application/json','Content-Type':'text/json','X-SyncTimeout':'30'},
+        auth=('278dc7a7-4997-4105-91b8-d190d0d81b71','Wgt3j8IMcOl0')
+        )
+    print r
+    try:
+        print r.text
+    except:
+        pass
 
 if __name__ == '__main__':
-    test()
-    #app.debug = True
-    #app.run(host='0.0.0.0', port=8000)
+    #test2()
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
